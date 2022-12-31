67a9374a6d9e9bcf0cc90086bb1840531ee9b23b
Julien Bourseau <Julien.Bourseau@grenoble-inp.org>
2022-12-31 17:34:13 +0100

    packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import Head from "next/head"
+import { MoralisProvider } from "react-moralis"
+import Header from "../components/Header"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="A decentralized NFT Marketplace" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <lifirefnk rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider initializeOnMount={false}>
+                <Header />
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </div>
+    )
 }
